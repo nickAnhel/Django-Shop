daa240ddee52db1f0a28450c7fb50c4aa806b6a1
nickAnhel <anhimovn1@gmail.com>
2024-03-08 19:01:53 +0300

    Ch.8 Add checkout

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+from .models import OrderItem
+from .forms import OrderCreateForm
+
+
+def order_create(request: HttpRequest) -> HttpResponse:
+    cart = Cart(request)
+
+    if request.method == "POST":
+        form = OrderCreateForm(data=request.POST)
+
+        if form.is_valid():
+            order = form.save()
+
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["quantity"],
+                )
+
+            cart.clear()
+            return render(request, "orders/order/created.html", {"order": order})
+    else:
+        form = OrderCreateForm()
+
+    return render(request, "orders/order/create.html",                                                                                                                       {"form": form})
