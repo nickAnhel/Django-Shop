c552257b0cc652b36e474990c6d1e3f9652d122f
nickAnhel <anhimovn1@gmail.com>
2024-03-15 23:08:13 +0300

    Ch.10 Add coupon apply form, view and path

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponseRedirect
+from django.shortcuts import redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .models import Coupon
+from .forms import CouponAplyForm
+
+
+@require_POST
+def coupon_apply(request: HttpRequest) -> HttpResponseRedirect:
+    now = timezone.now()
+    form = CouponAplyForm(request.POST)
+
+    if form.is_valid():
+        code = form.changed_data["code"]
+
+        try:
+            coupon = Coupon.objects.get(
+                code__iexact=code, valid_from__lte=now, valid_to__gte=now, active=True
+            )
+            request.session["coupon_id"] = coupon.id
+        except Coupon.DoesNotExist:
+            request.session["coupon_id"] = None
+
+    return redirect("cart:cart_detail")
